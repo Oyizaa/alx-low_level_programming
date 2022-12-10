e31ed37971aa902a44692485b88d09b247bb9e42
Oyizaa <musaoyizarahama@gmail.com>
2022-12-09 20:15:24 -0800

    c basic

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,11 @@
  * */
 int main(void)
 { 	      
-      	int;
+      	int n;
 
 	srand(time(0));
 	        n = rand() - RAND_MAX / 2;
-		        if (n>0)
+		        if (n > 0)
 			{
 				printf("%li is positive\n", (long int)n);
 			}
