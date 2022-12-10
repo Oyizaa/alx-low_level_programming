6c7f3af602de17b48840afc806852ffe6ad4c5d1
Oyizaa <musaoyizarahama@gmail.com>
2022-12-10 14:16:05 +0000

    alx low programming

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-        int n;
+    	int n;
         int a;
 
-        srand(time(0));
+      	srand(time(0));
         n = rand() - RAND_MAX / 2;
         a = n % 10;
         if (a > 5)
